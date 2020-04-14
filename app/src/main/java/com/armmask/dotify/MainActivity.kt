0068578232a4a2241bda4b701d0a84d0776bedec
Arman Kazi <armmask@uw.edu>
2020-04-13 17:44:34 -0700

    commit three

--- app/src/main/java/com/armmask/dotify/MainActivity.kt
+++ app/src/main/java/com/armmask/dotify/MainActivity.kt
@@ -2,11 +2,16 @@ package com.armmask.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeUsername(view: View) {
+
     }
 }
