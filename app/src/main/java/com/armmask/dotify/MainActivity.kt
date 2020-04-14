9cca3adf423926f562f8d556b78a295cbb401649
Arman Kazi <armmask@uw.edu>
2020-04-13 21:41:09 -0700

    commit 5: ec done. #1, #4, #5

--- app/src/main/java/com/armmask/dotify/MainActivity.kt
+++ app/src/main/java/com/armmask/dotify/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.armmask.dotify
-
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private var randNum = 0
+    private var color = R.color.colorBlack
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         randNum = Random.nextInt(0, 9999999)
         val tvPlayCount = findViewById<TextView>(R.id.tvPlayCount)
         tvPlayCount.text = "$randNum plays"
+        val imgAlbum = findViewById<ImageView>(R.id.imgAlbumCover)
     }
+
+
 
     fun changeUsername(view: View) {
         val tvUsername = findViewById<TextView>(R.id.tvUsername)
@@ -25,6 +29,11 @@ class MainActivity : AppCompatActivity() {
         val btnChangeUser = findViewById<Button>(R.id.btnChangeUser)
         if (evUsername.visibility == View.VISIBLE) {
             val username = evUsername.text
+            if (username.isBlank() || username.isEmpty()) {
+                Toast.makeText(this, "Please enter a username",
+                    Toast.LENGTH_SHORT).show()
+                return
+            }
             tvUsername.text = username
             evUsername.visibility = View.GONE
             tvUsername.visibility = View.VISIBLE
